7519acc94210ef1385a100817389255674e8a4bf
unknown <antonioprudente04@gmail.com>
2026-09-07 15:17:02 +0200

    Fix cors config

--- config/cors.go
+++ config/cors.go
@@ -6,19 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// CORSMiddleware gestisce le intestazioni CORS per un dominio specifico
+// CORS gestisce le intestazioni CORS per il frontend React
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Sostituisci con l'origine esatta da cui vuoi accettare le richieste
-		allowedOrigin := "http://localhost:5173"
+		allowedOrigin := "http://localhost:5174" // Sostituisci con l'URL esatto del tuo frontend
 
 		c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
+		// Gestione della richiesta Preflight OPTIONS
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent) // 204 No Content
 			return
 		}
 
